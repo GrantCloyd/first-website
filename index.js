23f5db934ee2ab46a71d4d86368740fc0df3b55b
GrantCloyd <dgcloyd@gmail.com>
2021-04-18 09:01:31 -0500

    Javascript begins - buttons created and appended

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-console.log("Hello World");
+console.log("Hello! I doubt many devs will take a look at this page given it's purpose - but thank you for stopping by!");
 
-const buttons = document.querySelectorAll("button");
+const calcLocation = document.querySelector("#interactiveText p");
+const mathSymbols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "-", "+", "*", "/", "=", "Cl"];
 
+const calcMaker = () => {
+    let i = 0;
+    while (i < mathSymbols.length) {
+        const newButton = document.createElement("button");
+        newButton.classList = "button mathButton";
+        newButton.innerHTML = mathSymbols[i];
+
+        if (typeof (mathSymbols[i]) === "number") {
+            newButton.value = mathSymbols[i];
+            newButton.addEventListener("click", () => {
+                alert(`${newButton.innerHTML}`)
+            });
+        } else {
+            newButton.addEventListener("click", function () {
+                alert(`Math`);
+            });
+        }
+        calcLocation.appendChild(newButton);
+        i++;
+    }
+}
+
+calcMaker();
