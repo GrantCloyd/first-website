7937bf0931a5bd977e6b0a716934d96d0943a353
GrantCloyd <dgcloyd@gmail.com>
2021-04-22 14:30:39 -0500

    insults added

--- insult.js
+++ insult.js
@@ -1,16 +1,26 @@
 
 //the insult arrays - taken from the Shakespeare Insult Kit
-const insultSet1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven",
+const insultSet1 = [
+    "artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven",
     "currish", "dankish", "dissembling", "droning", "errant", "fawning", "fobbing", "froward", "frothy", "gleeking", "goatish",
-    "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling"];
+    "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling",
+    "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly",
+    "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"
+];
 const insultSet2 = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained",
     "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned",
     "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced",
-    "hasty-witted", "hedge-born", "hell-hated", "idle-headed"];
-const insultSet3 = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish"
+    "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered",
+    "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed",
+    "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"
+];
+const insultSet3 = [
+    "apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish"
     , "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet",
-    "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster"];
-
+    "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm",
+    "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion",
+    "ratsbane", "whey-face", "wagtail", "scut", "skainsmate", "strumpet", "varlot", "vassal"
+];
 
 //generates 3 random numbers that cap based on the length of the first array and pushes the genearted numbers to a new array
 // currently insults need to be added in threes - one to each array at a time - but they can be added to the array without needing to tweak the function   
@@ -27,7 +37,7 @@ function insulterizer(adj1Array, adj2Array, nounArray) {
     let insult1 = adj1Array[index[0]];
     insult2 = adj2Array[index[1]];
     insult3 = nounArray[index[2]];
-    return `Thou ${insult1} ${insult2} ${insult3}`;
+    return `Thou ${insult1} ${insult2} ${insult3}!`;
 }
 
 //building the location and objects to place the features
@@ -44,7 +54,7 @@ Object.assign(insultScreen, {
     class: "button",
     disabled: true,
     id: "insultText",
-    value: "Prepare thyself ... insults are afoot!"
+    value: '"By the pricking of my thumbs, something insulting this way comes"'
 });
 insultDiv.appendChild(insultScreen);
 
@@ -61,3 +71,6 @@ insultDiv.appendChild(insultButton);
 
 console.log(insulterizer(insultSet1, insultSet2, insultSet3))
 
+console.log(insultSet1.length)
+console.log(insultSet2.length)
+console.log(insultSet3.length)
