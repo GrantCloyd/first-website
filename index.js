f53bc658e5c4f2ada04a15385d606ea952b79044
GrantCloyd <dgcloyd@gmail.com>
2021-04-21 21:12:18 -0500

    modulo and exponent button added, js description added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 console.log("Hello! I doubt many devs will take a look at this page given it's purpose - but thank you for stopping by!");
-
+//setting a location for the calculator
 const calcLocation = document.querySelector("#interactiveText p");
+//for loop seemed excessive to create two hr elements
 const hr1 = document.createElement("hr");
 const hr2 = document.createElement("hr");
 calcLocation.appendChild(hr1);
+//screen creation and object parameters set; disabled input button with the value feature allows a text to appear that can't be typed on/over
 const calcScreen = document.createElement("input");
 Object.assign(calcScreen,
     {
@@ -18,17 +20,20 @@ Object.assign(calcScreen,
     })
 
 calcLocation.appendChild(calcScreen);
-const mathSymbols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "-", "+", "*", "/", "=", "Cl"];
-
-
+//array created/used to pass the neccessary button symbols to the arr parameter in calcMaker
+const mathSymbols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "-", "+", "*", "/", "=", "**", "%", "Cl"];
+//once the function is built, what's nice is that I can pass other javascript operators to the array and it adds it immediately 
+// for example, in the original incarnation I did not have the exponent option or the modulo - but after construction, just by adding them to the array as a string, they populate, append, and function immediately
 
 function calcMaker(arr) {
     let i = 0;
     while (i < arr.length) {
+        //creates a generic button and sets text, values, etc
         const newButton = document.createElement("button");
         newButton.classList = "button mathButton";
         newButton.innerHTML = mathSymbols[i];
         newButton.value = mathSymbols[i];
+        //if and else if flow to create variations based on a number, equal sign, clear, or operator. All buttons store values on the calcScreen object
         if (typeof (mathSymbols[i]) === "number") {
             newButton.addEventListener("click", () => {
                 calcScreen.value += `${newButton.value}`;
@@ -41,11 +46,13 @@ function calcMaker(arr) {
                 calcScreen.mathMethod = null;
             });
         }
+
         else if (mathSymbols[i] === "=") {
             newButton.id = "equalSign";
             newButton.addEventListener("click", () => {
                 calcScreen.storedValue2 = calcScreen.value;
-                calcScreen.value = eval(`${Number(calcScreen.storedValue1)} ${calcScreen.mathMethod} ${Number(calcScreen.storedValue2)}`);
+                //not sure if this is the best approach, in building this I built myself into a corner and googled and found eval to solve the problem as it evaluates a string and returns the number
+                calcScreen.value = eval(`${calcScreen.storedValue1} ${calcScreen.mathMethod} ${calcScreen.storedValue2}`);
                 calcScreen.storedValue1 = null;
                 calcScreen.storedValue2 = null;
             })
@@ -62,8 +69,6 @@ function calcMaker(arr) {
         calcLocation.appendChild(newButton);
         i++;
     }
-
-
 
 }
 calcMaker(mathSymbols);
