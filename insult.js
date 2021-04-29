218e93d5b54f75b86abea3f7de3518463bf02453
GrantCloyd <dgcloyd@gmail.com>
2021-04-29 14:05:12 -0500

    hide removed from insult.js

--- insult.js
+++ insult.js
@@ -76,21 +76,3 @@ insultDiv.appendChild(insultButton);
 console.log(insulterizer(insultSet1, insultSet2, insultSet3))
 
 
-//hide functionality for the learning/work log
-
-const hiddenL = document.querySelector("#hideLearn");
-const hiddenW = document.querySelector('#hideWork');
-const dropDownLearning = document.querySelector('#learningDropDown');
-const dropDownWork = document.querySelector('#workDropDown');
-
-const hide = (hidden) => {
-    if (hidden.style.display === "none") {
-        hidden.style.display = "block";
-        hidden.classList = "transition";
-    } else {
-        hidden.style.display = "none";
-    }
-}
-
-dropDownLearning.addEventListener("click", () => hide(hiddenL));
-dropDownWork.addEventListener("click", () => hide(hiddenW));
